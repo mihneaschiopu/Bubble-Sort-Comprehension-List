080a7d18b26a980ad2ba075ea68713e9702d950e
Mihnea <mihnea@myownsummer.co.uk>
2025-11-24 10:14:58 +0000

    Add files via upload

---
+++ bubble in comprehension list.py
@@ -0,0 +1,30 @@
+# conditional list
+
+
+
+import random
+y = 10
+items = [random.randint(0,100) for x in range(y)]
+n= len(items)-1
+swapped = True
+
+
+while swapped and n > 0: # External Loop
+    swapped = False   # breaks loop if swapped assigned to false  
+    for index in range(0, n):
+        if (items[index] > items[index+1]):  # if statement for item 0 to 1
+            temp = items[index] # defining temp as index
+            items[index] = items[index+1]  # item 0 has been assigned to item 1
+            items[index+1] = temp  # item 1 becomes item 0
+
+            swapped = True
+
+
+    n = n - 1
+
+        
+        
+    
+
+print(items)
+        
